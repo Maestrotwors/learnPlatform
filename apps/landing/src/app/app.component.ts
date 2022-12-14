12bd788d419dfadfe0c51db53dea48fd2bddae5a
RomanRakzin <>
2022-12-14 22:58:53 +0100

    tailwind added

--- apps/landing/src/app/app.component.ts
+++ apps/landing/src/app/app.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'learnplatform-root',
   template: '<router-outlet></router-outlet>',
+  styleUrls: ['./remote-entry/styles.scss'],
 })
 export class AppComponent {}
--- apps/landing/src/app/remote-entry/pages/index/index.module.ts
+++ apps/landing/src/app/remote-entry/pages/index/index.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { HeaderSectionComponent } from './components/header-section/header-section.component';
 import { IndexComponent } from './index.component';
 import { IndexRoutingModule } from './index-routing.module';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, HeaderSectionComponent],
   imports: [CommonModule, IndexRoutingModule],
 })
 export class IndexModule {}
